ce0b1f32a99b13565a1f1dbdf7dc585dbdab885b
Prash-Python <prash.javadev1@gmail.com>
2025-12-04 21:33:17 +0530

    Add files via upload

--- Java_Python_Programming/starters/problem3/compare_two_numbers.py
+++ Java_Python_Programming/starters/problem3/compare_two_numbers.py
@@ -1,14 +1,14 @@
 # Compare two numbers and print the larger one or indicate if they are equal.
-# def compareTwoNumbers(a, b):
-#     if a > b:
-#         print(f"{a} is greater than {b}")
-#     elif b > a:
-#         print(f"{b} is greater than {a}")
-#     else:
-#         print("Both numbers are equal")
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
-# compareTwoNumbers(num1, num2)
+def compareTwoNumbers(a, b):
+    if a > b:
+        print(f"{a} is greater than {b}")
+    elif b > a:
+        print(f"{b} is greater than {a}")
+    else:
+        print("Both numbers are equal")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+compareTwoNumbers(num1, num2)
 
 def compareGivenNumbers(a, b):
     if a == b:
